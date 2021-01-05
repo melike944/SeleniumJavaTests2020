9a9a8357d06a7e5c6be610b3d3a2f7eef25600be
asus <xahidesufiya@gmail.com>
2021-01-05 13:26:01 +0300

    JunitTestDemo

--- src/test/java/com/unitedcoder/regression/uitest/junittest/JunitTetDemo.java
+++ src/test/java/com/unitedcoder/regression/uitest/junittest/JunitTetDemo.java
@@ -1,23 +1,47 @@
 package com.unitedcoder.regression.uitest.junittest;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.rules.TestName;
+import org.junit.runners.MethodSorters;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class JunitTetDemo {
+    @BeforeClass
+    public static void setup(){
+        System.out.println("Before class runs only once");
+    }
+    @Rule
+    public TestName name=new TestName();
+    @Before
+    public  void testBegin(){
+        System.out.println("Before annotaion will execute before every test ");
+        System.out.println(name.getMethodName()+" started");
+    }
+    // @Ignore
     @Test
-    public void test1(){
-        System.out.println("square root test");
+    public void edit(){
+        System.out.println("Square root test");
         Assert.assertTrue(Math.sqrt(64)==8);
     }
     @Test
-    public void test2(){
-        System.out.println("compare two string ");
-        String s1="java";
-        String s2="jawa";
+    public void delete(){
+        System.out.println("Compare Two String");
+        String s1="Selenium";
+        String s2="selenium";
         Assert.assertFalse(s1.equals(s2));
     }
     @Test
-    public void test3(){
+    public void addCustomer(){
+        System.out.println("Compare Two Numbers");
         Assert.assertEquals(10,10);
     }
+    @After
+    public void testEnd(){
+        System.out.println("After annotation will execute after every test");
+    }
+    @AfterClass
+    public static void teardown(){
+        System.out.println("After class will execute once after test");
+    }
 }
+
