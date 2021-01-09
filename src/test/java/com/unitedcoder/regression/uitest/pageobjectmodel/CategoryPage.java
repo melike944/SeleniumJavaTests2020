953a39461d6534674546349843c54e7b5dde4b55
asus <xahidesufiya@gmail.com>
2021-01-09 22:55:22 +0300

    page object model

---
+++ src/test/java/com/unitedcoder/regression/uitest/pageobjectmodel/CategoryPage.java
@@ -0,0 +1,30 @@
+package com.unitedcoder.regression.uitest.pageobjectmodel;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CategoryPage {
+    WebDriver driver;
+    TestUtility utility;
+    @FindBy(css="i.fa.fa-trash")
+    WebElement deleteIcon;
+    @FindBy(xpath = "//div[@class='success']")
+    WebElement successfullyDeletedMessage;
+
+    public CategoryPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+        utility=new TestUtility(driver);
+    }
+    public boolean deleteCategory(){
+        utility.waitForElementPresent(deleteIcon);
+        deleteIcon.click();
+        utility.waitForAlertPresent();
+        Alert alert=driver.switchTo().alert();
+        alert.accept();
+        return successfullyDeletedMessage.isDisplayed();
+    }
+}
---
+++ src/test/java/com/unitedcoder/regression/uitest/pageobjectmodel/CustomerPage.java
@@ -0,0 +1,30 @@
+package com.unitedcoder.regression.uitest.pageobjectmodel;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class CustomerPage {
+    WebDriver driver;
+    TestUtility utility;
+    @FindBy(xpath = "//div[@id='customer-list']/table")
+    WebElement customerTable;
+    @FindAll(
+            @FindBy(xpath = "//div[@id='customer-list']/table/tbody/tr")
+    )
+    List<WebElement> customerList;
+
+    public CustomerPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+        utility=new TestUtility(driver);
+    }
+    public boolean viewCustomers(){
+        utility.waitForElementPresent(customerTable);
+        return customerList.size()>=1;
+    }
+}
